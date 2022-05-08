ee642123cbc1b95cdb76e6a47daf4c507d288cde
Ponlapat Ittiratchapong <webmaster@maxnus.com>
2022-05-08 16:30:48 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CN311_Platforming_Final_Project_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
