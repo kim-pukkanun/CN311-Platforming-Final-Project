63249827cc544c3b3c39be94aa99e345f75de424
kim-pukkanun <pukkanun.som@dome.tu.ac.th>
2022-05-22 11:07:06 +0700

    add check playerID for PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,9 +16,19 @@ public class PlayerController : MonoBehaviour
     private float moveY;
     private float rotateDeath;
 
-    public bool isDisable;
-    public bool isDeathCount;
+    public string playerID;
 
+    public bool isDisable = false;
+    public bool isSocket = false; 
+    public bool isDeathCount = false;
+
+
+    private void Awake()
+    {
+        if (playerID != gameObject.name) {
+            isSocket = true;
+        }
+    }
 
     // Start is called before the first frame update
     private void Start()
@@ -29,10 +39,6 @@ public class PlayerController : MonoBehaviour
         moveSpeed = 1f;
         jumpForce = 20f;
         isJumping = false;
-
-        isDisable = false;
-        isDeathCount = false;
-
     }
 
     // Update is called once per frame
@@ -40,46 +46,50 @@ public class PlayerController : MonoBehaviour
     {
         moveX = Input.GetAxisRaw("Horizontal");
         moveY = Input.GetAxisRaw("Vertical");
+
+        // gameObject.transform.position = new Vector3(x, y, 0);
     }
 
     private void FixedUpdate() 
     {
-        #region walk_jump
-        if ((moveX > 0.1f || moveX < -0.1f) && !isDisable) {
-            rb2D.AddForce(new Vector2(moveX * moveSpeed, 0f), ForceMode2D.Impulse);
-        }
+        if (!isSocket) {
+            #region walk_jump
+            if ((moveX > 0.1f || moveX < -0.1f) && !isDisable) {
+                rb2D.AddForce(new Vector2(moveX * moveSpeed, 0f), ForceMode2D.Impulse);
+            }
 
-        if (!isJumping && moveY > 0.1f && !isDisable) {
-            rb2D.AddForce(new Vector2(0f, moveY * jumpForce), ForceMode2D.Impulse);
-        }
-        #endregion
+            if (!isJumping && moveY > 0.1f && !isDisable) {
+                rb2D.AddForce(new Vector2(0f, moveY * jumpForce), ForceMode2D.Impulse);
+            }
+            #endregion
 
-        #region death
-        rotateDeath = gameObject.transform.localRotation.eulerAngles.z;
+            #region death
+            rotateDeath = gameObject.transform.localRotation.eulerAngles.z;
 
-        if (((rotateDeath > 85 && rotateDeath < 95) || (rotateDeath < 280 && rotateDeath > 270) || (rotateDeath > 179 && rotateDeath < 181)) && !isDisable && !isDeathCount) {
-            Debug.Log("Enter on " + rotateDeath);
-            Thread t1 = new Thread(OnDeath);
-            t1.Start();
-            isDeathCount = true;
-        }
+            if (((rotateDeath > 85 && rotateDeath < 95) || (rotateDeath < 280 && rotateDeath > 270) || (rotateDeath > 179 && rotateDeath < 181)) && !isDisable && !isDeathCount) {
+                Debug.Log("Enter on " + rotateDeath);
+                Thread t1 = new Thread(OnDeath);
+                t1.Start();
+                isDeathCount = true;
+            }
 
-        if (isDisable) {
-            SceneManager.LoadScene(1);
-        }
-        #endregion
+            if (isDisable) {
+                SceneManager.LoadScene(1);
+            }
+            #endregion
 
-        #region animCheck
-        if (moveX > 0.1f && !isDisable) {
-            animator.SetBool("isRunning", true);
-            gameObject.transform.localScale = new Vector2(1, 1);
-        } else if (moveX < -0.1f && !isDisable) {
-            animator.SetBool("isRunning", true);
-            gameObject.transform.localScale = new Vector2(-1, 1);
-        } else {
-            animator.SetBool("isRunning", false);
+            #region animCheck
+            if (moveX > 0.1f && !isDisable) {
+                animator.SetBool("isRunning", true);
+                gameObject.transform.localScale = new Vector2(1, 1);
+            } else if (moveX < -0.1f && !isDisable) {
+                animator.SetBool("isRunning", true);
+                gameObject.transform.localScale = new Vector2(-1, 1);
+            } else {
+                animator.SetBool("isRunning", false);
+            }
+            #endregion
         }
-        #endregion
     }
 
     private void OnDeath() 
